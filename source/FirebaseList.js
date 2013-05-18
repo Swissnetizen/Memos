c135e67e6441bc07f0cdd9c7916af1d5a4d14ee4
Samarth's Dashboard <samu.agarwal@gmail.com>
2013-05-18 09:28:00 -0400

    Get child_add event handler working

--- source/FirebaseList.js
+++ source/FirebaseList.js
@@ -1,4 +1,3 @@
-
 enyo.kind({
     //*@public
     /** Sam.FirebaseList takes a url reference to the firebase
@@ -9,65 +8,82 @@ enyo.kind({
     kind: "enyo.List",
     name: "Sam.FirebaseList",
     published: {
-        //The url to the firebase
-    firebase: undefined
+        //*The url to the firebase
+        firebase: undefined,
+        /** navTree, a function which navigates the current array location. 
+            Arguments given: data in the location.
+            Result: the id.
+            By default it is set to return the argument. */    
+        navTree: function(d) { return d;}
     },
     //*@protected
+    //* Actual data that is stored. Data is rendered in the reverse order.
     data: [],
         instance: undefined,
         create: function() { 
             this.inherited(arguments);
             this.firebaseChanged();
         },
-        //Asigns onSetuItem to setupItem; needed to inject data into event,
+        //*Assigns onSetupItem to setupItem; needed to inject data into event,
         handlers: {
             onSetupItem: "setupItem"
         },
         events: {
+            //* Like an enyo.List's onSetupItem;
+            //* inEvent.data contains the firebase data for the current location.
             onItemSetup: ""
         },
         setupItem: function(inSender, inEvent) {
-            inEvent.data = this.data[inEvent.index];
-            this.doItemSetup(inEvent);  
+            // *Reverses the render order without changing this.data
+            // *which array.reverse() does.
+            inEvent.data = this.data[this.data.length-inEvent.index];
+            //Injects inEvent with the new data.
+            this.doItemSetup(inEvent);
+            return true;
         },
         //* Resets the firebase instance and list.
         firebaseChanged: function () {
             if (this.firebase) {
                 this.instance = new Firebase(this.firebase);
-                this.instance.on("child_added", this.childAdd);
-                this.instance.on("child_changed",this.childChange);
-                this.nstance.on("child_moved", this.childMove);
-                this.instance.on("child_removed", this.childRemove);
+                this.instance.on("child_added", enyo.bind(this, "childAdd"));
+                // this.instance.on("child_changed", this.childChange);
+                // this.nstance.on("child_moved", this.childMove);
+                // this.instance.on("child_removed", this.childRemove);
             } else {
                 enyo.throw("Sam.FirebaseList: Something's wrong with the firebase url.");
             }
+            this.data = [];
+            this.reset();
             return;
         },
-        //* Finds an array location with the specified id, not very _efficent_.
-        findArrayLocationByItemId: function(array, id) {
-            for (var i=0; i <= array.length; i+1) {
-                if (array[i].id === id) {
+        /** Finds an array location with the specified id, not very _efficent_.
+         Accepts 3 arguments:
+            1. Data in which should be searched. Accepts Arrays.
+            2. Id which should be found. Accepts any datatype that can be 
+                compared using the === operator(basically everyting).
+            3. navTree, a function which navigates the current array location, 
+                is given the, data in the location and should return the id of 
+                the location.
+         */
+        findArrayLocationByItemId: function(array, id, navTree) {
+            for (var i=0; i <= array.length; i+=1) {
+                if (navTree(array[i]) === id) {
                     return i;
-                } 
+                }
             }  
         },
         //* Handles `child_added` events; adds the child to the array.
         childAdd: function(snapshot, prevChildName) {
             var data = snapshot.val().id = snapshot.name();
-            var prevChildListId;
-            if (prevChildName === null) {
-                prevChildListId = this.findArrayLocationByItemId(this.data, prevChildName);
-            } else {
-                prevChildListId = -1; //* Sets the id to minus 1, so it's zero when spliced into the array;
-            }
-            this.data.splice(prevChildListId+1, 0, data);            
+            var prevChildListId = prevChildName !== null ? this.findArrayLocationByItemId(this.data, prevChildName, this.navTree) : -1;
+            this.data.splice(prevChildListId+1, 0, data);
+            this.count = this.data.length;
             this.refresh();
         },
         //*
-        child_changed: function() {,
+        child_changed: "",
         //*
         child_moved: "",
         //*
         child_removed: "",
-        
-    });    
+    });
