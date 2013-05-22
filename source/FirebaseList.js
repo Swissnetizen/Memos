c60906806bf9eac37f7671d7b3014247db99c87f
Samarth's Dashboard <samu.agarwal@gmail.com>
2013-05-22 07:46:34 -0400

    Switched from throw to warn

--- source/FirebaseList.js
+++ source/FirebaseList.js
@@ -87,7 +87,7 @@ enyo.kind({
             this.instance.on("child_removed", enyo.bind(this, "childRemove"));
             this.instance.on("child_moved", enyo.bind(this, "childMove"));
         } else {
-            throw("Sam.FirebaseList: Something's wrong with the firebase url.");
+            console.warn("Sam.FirebaseList: Something's wrong with the firebase url.");
         }
         this.data = [];
         this.reset();
