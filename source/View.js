114646033c131a43d43ec4d1cff461aa23608eb0
Samarth's Dashboard <samu.agarwal@gmail.com>
2013-05-23 13:28:29 -0400

    Finished newbutton functionality

--- source/View.js
+++ source/View.js
@@ -7,28 +7,45 @@ enyo.kind({
     published: {
         //*Firebase URL
         firebase: "",
+        onItemSetup: "",
     },
     events: {
-        onItemSetup: "",
         onItemOpen: "",
     },
     //@protected
     create: function() {
         this.inherited(arguments)
+        this.firebaseChanged()
+    },
+    firebaseChanged: function() {
         this.$.list.set("firebase", this.firebase);
+        return true;
     },
     components: [
         {kind: "onyx.Toolbar", name: "header", content: "Labels"},
+        {kind: "Sam.NewDialogue", name: "newDialogue", onConfirm: "continueNewDialogue"},
         {kind: "Sam.FirebaseList", name: "list", fit: true, onItemSetup: "setupItem", components: [
-            {kind: "onyx.Item", components: [
+            {kind: "onyx.Item", ontap: "sendItemTap", components: [
                 {name: "name", tag: "enyo.Control", style: "float: left"},
                 {name: "count", tag: "enyo.Control", style: "float: right"}
             ]}
         ]},
-        {kind: "Sam.FooterBar", name: "footer"}
+        {kind: "Sam.FooterBar", name: "footer", onNewTap: "newTap", onFindTap: "findTap", onEditTap: "editTap"}
     ],
-    setupItem: function (inSender, inEvent) {
+    setupItem: function(inSender, inEvent) {
+        var data = inEvent.data;
+        this.$.name.set("content", data.name);
+        this.$.count.set("content", data.count);
+    },
+    sendItemTap: function(inSender, inEvent) {
         inEvent.originalSender = inSender.name;
-        enyo.bind(this, "doItemSetup")(inEvent);
+        this.doItemOpen(inEvent);
+    },
+    newTap: function(inSender, inEvent) {
+        this.$.newDialogue.show()
+    },
+    continueNewDialogue: function (inSender, inEvent) {
+        this.$.list.push({name: inEvent.value, count: 0});
+        return true;
     }
 });
