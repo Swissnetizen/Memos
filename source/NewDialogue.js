441314c4f8c99a62b6cc083b2b4ba70170d24130
Samarth's Dashboard <samu.agarwal@gmail.com>
2013-05-23 13:41:14 -0400

    New styling & fix old text not deleting itself

--- source/NewDialogue.js
+++ source/NewDialogue.js
@@ -1,7 +1,8 @@
 enyo.kind({
-    kind: "enyo.Popup", 
+    kind: "onyx.Popup", 
     name: "Sam.NewDialogue", 
-    classes: "onyx", 
+    classes: "onyx",
+    style: "background: #eaeaea; color:#333333;",
     events: {
         onCancel: "",
         onConfirm: ""
@@ -10,28 +11,43 @@ enyo.kind({
         hideOnCanceled: true,
         confirmLabel: "Create"
     },
+    floating: true,
+    centered: true,
+    scrim: true,
+    create: function() {
+        this.inherited(arguments);
+        this.hideOnCanceledChanged();
+    },
+    hideOnCanceledChanged: function() {
+        this.$.confirmCreate.set("content", this.confirmLabel);
+    },
     components: [
-        {kind: "enyo.Control", name: "title", content: "New Catergory..."},
-        {kind: "enyo.Control", name: "line", style: "border-bottom: 1px solid rgba(0, 0, 0, 0.2);"},
-        {kind: "onyx.InputDecorator", name: "input", components: [
-            {kind: "onyx.Input", placeholder: "Enter some text..."}
+        {kind: "enyo.Control", name: "title", content: "New Catergory..."},{tag: "br"},
+        {kind: "enyo.Control", name: "line", style: "border-bottom: 1px solid rgba(0, 0, 0, 0.2); width:100%"},
+        {tag: "br"},
+        {kind: "onyx.InputDecorator", components: [
+            {kind: "onyx.Input", name: "input", placeholder: "Enter some text..."}
         ]},
-        {kind: "onyx.Button", name: "confirmCreate", content: this.confirmLabel, classes: "onyx-affermitive", ontap: "continue"},
-        {kind: "onyx.Button", name: "confirmCreate", content: "Cancel", ontap: "DoHide"}
+        {tag: "br"},
+        {kind: "onyx.Button", name: "confirmCreate", classes: "onyx-affirmative", ontap: "continue",  style: "width:100%"},
+        {tag: "br"},
+        {kind: "onyx.Button", name: "cancelCreate", content: "Cancel", ontap: "DoHide", style: "width:100%;"}
     ],
-    continue: function(inSender, inEvent) {
-        inEvent.originalInSender = inSender;
+    continue: function() {
+        var inEvent = {};
         inEvent.value = this.$.input.hasNode().value;
+        this.$.input.hasNode().value = "";
         this.doConfirm(inEvent);
-        this.DoHide();
+        this.hide();
         return true;
     },
     DoHide: function() {
         if (this.hideOnCanceled === true) {
             this.hide()
-        } 
-        inEvent.originalInSender = inSender;
+        }
+        var inEvent = {};
         inEvent.value = this.$.input.hasNode().value;
+        this.$.input.hasNode().value = "";
         this.doCancel(inEvent);
         return true;
     }
